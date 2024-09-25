d2910e470503213784a20040fdbad9ab5fffcd59
EsmiraFarziyeva2004 <123585048+EsmiraFarziyeva2004@users.noreply.github.com>
2024-09-26 01:09:12 +0400

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,25 +1,28 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
-    
+
     @Test
-    public void testMain() {
+    public void testMainOutput() {
+        // Arrange
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream originalOut = System.out; 
+        System.setOut(new PrintStream(outputStream)); 
 
-        System.setOut(new PrintStream(outputStream));
-
+        // Act
         Main.main(new String[]{});
 
-        System.setOut(originalOut);
+        // Assert
+        assertEquals("Hello, world!\n", outputStream.toString()); 
 
-        String expectedOutput = "Hello world!\n"; 
-        assertEquals(expectedOutput, outputStream.toString());
+        // Restore original System.out
+        System.setOut(originalOut);
     }
 }
