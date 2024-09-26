d7ed687d138a03ba4a72d3aaa4f169d06b06d7e5
EsmiraFarziyeva2004 <123585048+EsmiraFarziyeva2004@users.noreply.github.com>
2024-09-26 19:35:31 +0400

    Update PermissionManager.java

--- src/main/java/PermissionManager.java
+++ src/main/java/PermissionManager.java
@@ -1,25 +1,10 @@
+
 public class PermissionManager {
-
     private PermissionLevel mCurrentLevel = PermissionLevel.USER;
-
-    public String getRoleName(PermissionLevel level) {
-        switch (level) {
-            case ADMIN:
-                return "Admin";
-            case DEVELOPER:
-                return "Developer";
-            case USER:
-                return "User";
-            default:
-                throw new IllegalArgumentException("Unknown PermissionLevel");
-        }
+    public String getRoleName() {
+        return mCurrentLevel.name();
     }
-
-    public void setCurrentLevel(PermissionLevel level) {
-        this.mCurrentLevel = level;
-    }
-
-    public PermissionLevel getCurrentLevel() {
-        return mCurrentLevel;
+    public void setRoleName(PermissionLevel level) {
+        mCurrentLevel = level;
     }
 }
